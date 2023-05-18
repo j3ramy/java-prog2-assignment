f4e6afb552a358c0b0f5c1a20834b7ba043b78b8
j3ramy <jeramy.gaming@gmail.com>
2023-05-18 16:23:05 +0200

    * ExceptionHandling

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,9 @@ public class Main {
     public static void main(String[] args) {
         //Create instance of app controller as an entry point for the MVC Pattern and init
         AppController appController = new AppController();
-        appController.init();
+        try {
+            appController.init();
+        }
+        catch(Exception ignored){}
     }
 }
